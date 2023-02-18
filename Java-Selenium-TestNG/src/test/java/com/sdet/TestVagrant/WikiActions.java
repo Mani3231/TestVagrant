79365bbcd0554c2629849a1326b9a93b7edf9916
PC <PC@DESKTOP-I71O81B>
2023-02-18 13:45:13 +0530

    This is TestVagrant

--- Java-Selenium-TestNG/src/test/java/com/sdet/TestVagrant/WikiActions.java
+++ Java-Selenium-TestNG/src/test/java/com/sdet/TestVagrant/WikiActions.java
@@ -3,6 +3,7 @@ package com.sdet.TestVagrant;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 
 import com.sdet.commons.Commons;
 
@@ -17,13 +18,19 @@ public class WikiActions {
 
 	public static boolean movieReleaseDate() {
 		WebElement releaseDate = driver.findElement(By.xpath(WikiHomePage.Release_Date));
-		System.out.println("Realese date : " + releaseDate.getText());
+		System.out.println(releaseDate.getText());
+		String expected = "Release date\r\n" + "17 December 2021";
+		String actual = driver.findElement(By.xpath(WikiHomePage.Release_Date)).getText();
+		Assert.assertNotNull(actual, expected);
 		return true;
 	}
 
 	public static boolean movieCountry() {
 		WebElement releaseCountry = driver.findElement(By.xpath(WikiHomePage.Country));
-		System.out.println("Realese Country : " + releaseCountry.getText());
+		System.out.println(releaseCountry.getText());
+		String expected = "Country India";
+		String actual = driver.findElement(By.xpath(WikiHomePage.Country)).getText();
+		Assert.assertEquals(actual, expected);
 		return true;
 
 	}
